5591eecb8ebeea7d2846147abe7a61967c70483b
awneet23 <awneet23@gmail.com>
2026-04-13 18:22:43 +0530

    production fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/chat_app/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/^(?!\/api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend","chat_app", "dist", "index.html"));
   });
 }
